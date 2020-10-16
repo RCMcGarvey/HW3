c5a6fa4f4e3084c4f8331780e1a7eac491a8a950
RCMcGarvey <38081008+RCMcGarvey@users.noreply.github.com>
2020-10-15 18:07:43 -0600

    Customer randomizer

--- src/Store.java
+++ src/Store.java
@@ -1,9 +1,9 @@
+import Customers.Customer;
 import Rolls.RollFactory;
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.Hashtable;
+import java.util.*;
 
 public class Store{
+    //All info on dictionaries was gathered from https://www.geeksforgeeks.org/java-util-dictionary-class-java/
     Store() {
         this.inventory.put("Spring Rolls", 30);
         this.inventory.put("Egg Rolls", 30);
@@ -12,6 +12,10 @@ public class Store{
         this.inventory.put("Jelly Rolls", 30);
 
     }
+
+    private Random rand = new Random();
+
+    //private CustomerFactory customerFactory = new CustomerFactory()
 
     Dictionary<String, Integer> inventory = new Hashtable<>();
 
@@ -28,7 +32,61 @@ public class Store{
     }
 
     public void runDay(){
-        
+        List<Customer> dailyCustomers = new ArrayList<>();
+        dailyCustomers = getCustomers();
+        for(int i = 0; i < dailyCustomers.size(); ++i){
+            //dailyCustomers[i].order
+        }
+
+
+
+
+    }
+
+    private List<Customer> getCustomers(){
+
+        int casuals = rand.nextInt(12)+1;
+        int business = rand.nextInt(3)+1;
+        int caterers = rand.nextInt(3)+1;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        while(!(a == casuals) &&  !(b == business) && !(c == caterers)){
+            int z = rand.nextInt(3);
+            int y = rand.nextInt(2);
+            if(z == 0){
+                if(a == casuals){
+                    if(y == 0){
+                        b++;
+                    } else {
+                        c++;
+                    }
+                }else {
+                    a++;
+                }
+            } else if(z == 1) {
+                if(b == business){
+                    if(y == 0){
+                        a++;
+                    } else {
+                        c++;
+                    }
+                }else {
+                    b++;
+                }
+            } else if(z == 2){
+                if(c == caterers){
+                    if(y == 0){
+                        a++;
+                    } else {
+                        b++;
+                    }
+                }else {
+                    c++;
+                }
+            }
+        }
+        return
     }
 
 
